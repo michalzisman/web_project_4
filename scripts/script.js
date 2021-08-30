8f16f204c08f62c5132bf4c194923e1b56b0dc04
michalzisman <michalzisman@gmail.com>
2021-08-30 22:28:17 +0300

    project5_roundTwo

--- scripts/script.js
+++ scripts/script.js
@@ -1,83 +1,81 @@
-let edit = document.querySelector(".profile__edit");
-let add = document.querySelector(".profile__add-button");
-let closeButton = document.querySelector(".popup__close");
-let popup = document.querySelector(".popup");
-let popupContainer = document.querySelector(".popup__container");
-let profileName = document.querySelector(".profile__name");
-let profileDescription = document.querySelector(".profile__description");
-let inputName = document.querySelector(".form__input_field_name");
-let inputDescription = document.querySelector(".form__input_field_description");
-let title = document.querySelector(".form__input_field_title");
-let link = document.querySelector(".form__input_field_link");
-let form = document.querySelector(".form");
-let imageLarge = document.querySelector(".imageLarge");
-let cards = document.querySelector(".cards");
-let cardTemplate = document.querySelector("#card").content;
-let submit = document.querySelector(".form");
+const cardTemplate = document.querySelector("#card").content;
+const addPlaceForm = document.querySelector(".popup_type_add-place");
+const editProfileForm = document.querySelector(".popup_type_edit-profile");
+const imagePopup = document.querySelector(".popup_type_image-popup");
+const largeImageData = imagePopup.querySelector(".imageLarge__image");
+const largeImageName = imagePopup.querySelector(".imageLarge__name");
+const cards = document.querySelector(".cards");
+const openEditProfileFormBtn = document.querySelector(".profile__edit");
+const openAddPlaceFormBtn = document.querySelector(".profile__add-button");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const popupAddPlace = document.querySelector(".popup_add-place");
+const inputName = document.querySelector(".form__input_field_name");
+const inputDescription = document.querySelector(".form__input_field_description");
+const inputImageTitle = document.querySelector(".form__input_field_title");
+const inputImageLink = document.querySelector(".form__input_field_link");
+const closeAddPlaceButton = document.querySelector(".popup__closeBtn_add-place");
+const closeEditProfileButton = document.querySelector(".popup__closeBtn_edit-profile");
+const closeImagePopup = document.querySelector(".popup__closeBtn_theme_image");
+const submitEditForm = document.querySelector(".form_type_edit-profile");
+const submitNewPlaceForm = document.querySelector(".form_type_add-place");
 
-function openPopup(event) {
-    if (event.target.className === "card__image") {
-        openImage(event);
-    } else {
-        openForm(event);
-    }
-    popupContainer.classList.add("popup__container_opened");
-    popup.style.opacity = "1";
+
+function openPopup(popup) {
     popup.classList.add("popup_opened");
 }
 
-function openImage(event) {
-    imageLarge.classList.add("imageLarge_opened");
-    closeButton.classList.add("popup__close_theme_image");
-    form.classList.remove("form_opened");
-    imageLarge.querySelector("img").src = event.target.currentSrc;
-    imageLarge.querySelector("img").alt = event.target.alt;
-    imageLarge.querySelector("p").textContent = event.target.alt;
+function closePopup(popup) {
+    popup.classList.remove("popup_opened");
 }
 
-function openForm(event) {
-    form.classList.add("form_opened");
-    closeButton.classList.remove("popup__close_theme_image");
-    imageLarge.classList.remove("imageLarge_opened");
-    if (event.target.ariaLabel==="Edit") {
-        form.querySelector(".form__heading").textContent = "Edit profile"
-        form.querySelector(".form__submit").textContent = "Save";
-        inputName.value = profileName.textContent;
-        inputDescription.value = profileDescription.textContent;
-    } else {
-        form.querySelector(".form__heading").textContent = "New place"
-        form.querySelector(".form__input_field_name").placeholder = "Title";
-        form.querySelector(".form__input_field_description").placeholder = "Image link";
-        form.querySelector(".form__submit").textContent = "Create";
-        inputName.value = "";
-        inputDescription.value = "";
-    }
+function openEditProfilePopup() {
+    inputName.value = profileName.textContent;
+    inputDescription.value = profileDescription.textContent;
+    openPopup(editProfileForm);
 }
 
-function closePopup() {
-    //using JS to change the style, as well as having a setTimeout for the modifier
-    //was the only way I was able to get a smooth effect for when the user closes the popup.
-    //I tried adding transition to the _opened modifier, but it didn't help.
-    setTimeout(function(){ popupContainer.classList.remove("popup__container_opened"); }, 200);
-    popup.style.opacity = "0";
-    popup.style.transition = "opacity 0.3s linear";
-    setTimeout(function(){ popup.classList.remove("popup_opened"); }, 300);
+function closeEditProfilePopup() {
+    closePopup(editProfileForm);
 }
 
-function submitForm(event) {
-    if (form.querySelector(".form__submit").textContent === "Create") {
-        const card = cloneCard(inputName.value, inputDescription.value)
-        cards.prepend(card);
-    } else {
-        profileName.textContent = inputName.value;
-        profileDescription.textContent = inputDescription.value;
-    }
+function openAddPlacePopup() {
+    openPopup(addPlaceForm);
+}
+
+function closeAddPlacePopup() {
+    inputImageTitle.value ="";
+    inputImageLink.value ="";
+    closePopup(addPlaceForm);
+}
+
+function openImagePopup(event) {
+    largeImageData.src = event.target.currentSrc;
+    largeImageData.alt = event.target.alt;
+    largeImageName.textContent = event.target.alt;
+    openPopup(imagePopup);
+}
+
+function closeLargeImagePopup() {
+    closePopup(imagePopup);
+}
+
+function submitEditProfileForm(event) {
+    profileName.textContent = editProfileForm.querySelector(".form__input_field_name").value;
+    profileDescription.textContent = editProfileForm.querySelector(".form__input_field_description").value;
     event.preventDefault();
-    closePopup();
+    closeEditProfilePopup();
+}
+
+function submitAddNewPlaceForm(event) {
+    const card = cloneCard(addPlaceForm.querySelector(".form__input_field_title").value, addPlaceForm.querySelector(".form__input_field_link").value)
+    cards.prepend(card);
+    event.preventDefault();
+    closeAddPlacePopup();
 }
 
 function toggleLike(event) {
-    return (event.target.className === "card__like" ? event.target.classList.add("card__like_active") : event.target.classList.remove("card__like_active"))
+    event.target.classList.toggle("card__like_active");
 }
 
 function deleteCard(event) {
@@ -86,47 +84,22 @@ function deleteCard(event) {
 
 function cloneCard(item, inputDescription) {
     const card = cardTemplate.cloneNode(true);
-    card.querySelector("img").src = inputDescription;
-    card.querySelector("img").alt = item;
-    card.querySelector("h2").textContent = item;
-    card.querySelector(".card__image").addEventListener("click", openPopup);
+    card.querySelector(".card__image").src = inputDescription;
+    card.querySelector(".card__image").alt = item;
+    card.querySelector(".card__title").textContent = item;
+    card.querySelector(".card__image").addEventListener("click", openImagePopup);
     card.querySelector(".card__like").addEventListener("click", toggleLike);
     card.querySelector(".card__delete").addEventListener("click", deleteCard);
     return card;
 }
 
-const initialCards = [
-    {
-      name: "Yosemite Valley",
-      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
-    },
-    {
-      name: "Lake Louise",
-      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
-    },
-    {
-      name: "Bald Mountains",
-      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
-    },
-    {
-      name: "Latemar",
-      link: "https://code.s3.yandex.net/web-code/latemar.jpg"
-    },
-    {
-      name: "Vanoise National Park",
-      link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
-    },
-    {
-      name: "Lago di Braies",
-      link: "https://code.s3.yandex.net/web-code/lago.jpg"
-    }
-];
-
 const items = initialCards.map(item => cloneCard(item.name, item.link));
-
 cards.append(...items);
 
-edit.addEventListener("click", openPopup);
-add.addEventListener("click", openPopup);
-closeButton.addEventListener("click", closePopup);
-submit.addEventListener("submit", submitForm);
+openEditProfileFormBtn.addEventListener("click", openEditProfilePopup);
+openAddPlaceFormBtn.addEventListener("click", openAddPlacePopup);
+closeAddPlaceButton.addEventListener("click", closeAddPlacePopup);
+closeEditProfileButton.addEventListener("click", closeEditProfilePopup);
+closeImagePopup.addEventListener("click", closeLargeImagePopup);
+submitEditForm.addEventListener("submit", submitEditProfileForm);
+submitNewPlaceForm.addEventListener("submit", submitAddNewPlaceForm);
